af55118a8634a7fb00d2c3c71255ea131a5e4be7
Wutthichai <wutthichai.kh@rmutsvmail.com>
2025-01-30 11:25:40 +0700

    Update contact.php

--- contact.php
+++ contact.php
@@ -2,7 +2,7 @@
     <table width="1238" height="530" border="0">
         <tr>
             <td width="23" rowspan="2">&nbsp;</td>
-            <td height="119" colspan="2" bgcolor="#006600">
+            <td height="119" colspan="2" bgcolor="#006600ccccccccccccccccccccccccccccccccccc">
                 <h1><center><font color="#FFFFFF">พรชัยอพาร์ตเมนต์</font></center></h1>
                 <?php include "Date.php"; ?>
             </td>
